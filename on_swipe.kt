0165a4de30d050ff34c496e4fc18e0647d7f4f89
AmatovaBegimai <amatova_b@auca.kg>
2021-11-21 21:13:36 +0600

    commit

---
+++ on_swipe.kt
@@ -0,0 +1,11 @@
+package com.example.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class on_swipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_swipe)
+    }
+}
